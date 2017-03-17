2e0487a8c55cc478613b6250aaf733ed8a7d2f67
jeffhoelter <jeffh@loar.net>
2017-03-17 15:34:36 -0400

    Episode 2 progress -- board rendering.

--- src/undead/client.cljs
+++ src/undead/client.cljs
@@ -1,3 +1,8 @@
 (ns undead.client)
 
-(js/alert "OOOHHHH")
+(def game {:board [{:face :h1} {:face :h1} {:face :h2} {:face :h2}
+                    {:face :h3} {:face :h3} {:face :h4} {:face :h4}
+                    {:face :h5} {:face :h5} {:face :fg} {:face :fg}
+                    {:face :zo} {:face :zo} {:face :zo} {:face :gy}]
+           :sand (repeat 30 :remaining)
+           :foggy false})
